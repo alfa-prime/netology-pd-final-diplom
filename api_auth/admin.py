949ed92eb46cc92da641dd8372ade4ec00e4f6d3
Alex Uglin <uglin76@mail.ru>
2022-04-28 09:35:27 +0300

    add User to admin panel

--- api_auth/admin.py
+++ api_auth/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from api_auth.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    User control panel
+    """
+    model = User
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'type')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'company', 'position')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_superuser')
